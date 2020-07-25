68de77b28b8641418c982f82333e2bb6af615dce
shendu1993 <e@onebook.club>
2020-07-25 15:51:36 +0800

    删除了all

--- Laravel-blog/app/Http/Controllers/DemoController.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: shendu
- * Date: 2020/5/22
- * Time: 22:44
- */
--- yhzihua.com/bootstrap/helpers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function route_class()
-{
-    return str_replace('.', '-', Route::currentRouteName());
-}
